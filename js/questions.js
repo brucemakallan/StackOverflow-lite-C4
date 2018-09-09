f9e9f981769d8090180b27af38dbfba9fea6faa3
Bruce Makaaru <brucemakallan@gmail.com>
2018-09-09 12:34:22 +0300

    Add fetch function for questions

---
+++ js/questions.js
@@ -0,0 +1,32 @@
+let showAlert = (error_msg) => {
+    let alertDiv = document.getElementById('alert-msg');
+    alertDiv.style.display = "block";
+    alertDiv.innerHTML = error_msg;  // show error message in alert box
+    console.log(error_msg);
+    setTimeout(() => alertDiv.style.display = "none", 7000);  // remove alert message after a while
+};
+
+
+// get a specific question OR all
+let getQuestions = (questionId=0) => {
+    // TODO change to proper route after testing
+    let url = 'https://stackoverflow-lite-abm.herokuapp.com/api/v1/questions';
+    if(questionId > 0)
+        url += '/' + questionId;
+
+    fetch(url, {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: {
+            'Content-type': 'application/json; charset=utf-8'
+            }
+        })
+        .then((response) => response.json)
+        .then((data) => console.log(JSON.stringify(data)))
+        .catch((error) => showAlert(error))
+};
+
+
+// let postQuestion = () => {
+//
+// };
---
+++ js/users.js
@@ -0,0 +1,40 @@
+let showAlert = (error_msg) => {
+    let alertDiv = document.getElementById('alert-msg');
+    alertDiv.style.display = "block";
+    alertDiv.innerHTML = error_msg;  // show error message in alert box
+    console.log(error_msg);
+    setTimeout(() => alertDiv.style.display = "none", 7000);  // remove alert message after a while
+};
+
+
+let signUp = (e) => {
+    e.preventDefault();  // prevent it from saving to a file
+
+    let username = document.getElementById('username').value;
+    let full_name = document.getElementById('full_name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let retype_password = document.getElementById('retype_password').value;
+
+    let inputData = {
+        username,
+        full_name,
+        email,
+        password,
+        retype_password
+    };
+
+    console.log(JSON.stringify(inputData));
+
+    fetch('https://stackoverflow-lite3-abm.herokuapp.com/api/v1/auth/signup', {
+            method: 'POST',
+            mode: "no-cors",  // no cross origin requests. Only HEAD, GET, and POST accepted
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: inputData
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(JSON.stringify(data)))
+        .catch((error) => showAlert(error))
+};
