82ab7cf60e75d3286630b65cd4f81724ae872610
Bruce Makaaru <brucemakallan@gmail.com>
2018-09-12 18:34:29 +0300

    Post answer, up-vote, down-vote, delete question

--- js/answers.js
+++ js/answers.js
@@ -1,8 +1,29 @@
-let showAlert = (error_msg) => {
-    let alertDiv = document.getElementById('alert-msg');
-    alertDiv.style.display = "block";
-    alertDiv.innerHTML = error_msg;  // show error message in alert box
-    console.log(error_msg);
-    setTimeout(() => alertDiv.style.display = "none", 7000);  // remove alert message after a while
+// Post an Answer
+let postAnswer = (e) => {
+    e.preventDefault();  // prevent it from saving to a file
+    let answer = document.getElementById('tx_answer').value;
+    let inputData = {
+        answer
+    };
+
+    fetch(`https://stackoverflow-lite3-abm.herokuapp.com/api/v1/questions/${localStorage.getItem("question_id")}/answers`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem("access_token")
+            },
+            body: JSON.stringify(inputData)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            let jsonStr = JSON.stringify(data);
+            if(jsonStr.includes("status_code"))  // response with "status_code" = something went wrong
+                showAlert(data.status_code);
+            else if(jsonStr.includes('"id":'))  // sucessful request
+                window.location.reload();
+        })
+        .catch((error) => showAlert(error))
 };
+
+
 
